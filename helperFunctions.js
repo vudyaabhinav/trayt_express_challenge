46fe3095253e200ea0adc1128a05f6f1678e72b8
Walter Wan <walter@trayt.io>
2020-09-08 18:35:49 -0700

    Add documentations

--- helperFunctions.js
+++ helperFunctions.js
@@ -27,6 +27,28 @@ const userRecommendations = {}
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
+/**
+ * @typedef Movie
+ * @property {string} id The id of the movie
+ * @property {string} name The name of the movie
+ * @property {string} director The director of the movie
+ * @property {string} geners An Array<string> of geners
+ */
+
+/**
+ * @typedef Recommendation
+ * @property {string} favDirector User's favorite director
+ * @property {string} favGenre User's favorite genre
+ * @property {<Array<Movie>} byDirector An array of movies directed by the same director
+ * @property {<Array<Movie>} byGenre An array of movies that are in the same genre
+ */
+
+/**
+ * Get user rated movies
+ * 
+ * @param  {string} userId user's id
+ * @return {Promise<Array<Movie>>} A promise to the token.
+ */
 async function getRatedMovies(userId) {
   const user = users[userId]
   if (!user) throw new Error('User does not exist')
@@ -34,24 +56,49 @@ async function getRatedMovies(userId) {
   return cloneDeep(ratedMovies[userId].map(({ id }) => movies[id]))
 }
 
+/**
+ * Get Recommendation by director
+ * 
+ * @param  {string} director director's name
+ * @return {Promise<Array<Movie>>} An array of recommended movies
+ */
 async function getRecommendationByDirector(director) {
   await delay(100)
   return cloneDeep(moviesByDirector[director].map((id) => movies[id]))
 }
 
+/**
+ * Get Recommendation by genre
+ * 
+ * @param  {string} genre
+ * @return {Promise<Array<Movie>>} An array of recommended movies
+ */
 async function getRecommendationByGenre(genre) {
   await delay(100)
   return cloneDeep(moviesByGenre[genre].map((id) => movies[id]))
 }
 
+/**
+ * Save Recommendation to database
+ * 
+ * @param  {string} userId User's id 
+ * @param  {Recommendation} recommendations Recommendation object that needs to be saved
+ * @return {Promise} returns nothing
+ */
 async function saveRecommendations(userId, recommendations) {
   const user = users[userId]
   if (!user) throw new Error('User does not exist')
   await delay(100)
   userRecommendations[userId] = cloneDeep(recommendations)
-  return { message: 'Recommendations saved successfully' }
+  return
 }
 
+/**
+ * Get Recommendation from database
+ * 
+ * @param  {string} userId
+ * @return {Promise<Recommendation>} recommendations Recommendation object that was saved to the database
+ */
 async function getSavedRecommendations(userId) {
   await delay(100)
   return cloneDeep(userRecommendations[userId])
